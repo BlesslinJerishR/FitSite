37325e45ce137e4a5cf1ebc5684a1d366ca3179e
Mastermindx33 <mastermindx33@gmail.com>
2021-07-27 19:14:04 +0530

    __dev__flex

--- js/script.js
+++ js/script.js
@@ -57,20 +57,8 @@ anime.timeline({loop: true})
     delay: 5000
   });
 
-var textWrapper = document.querySelector('.ml6 .text33');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 2000,
-    delay: (el, i) => 100 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 5000
-  });
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
