c804869f14be7e3b914b4431758d0ae7907f0c7b
Mastermindx33 <mastermindx33@gmail.com>
2021-07-27 13:39:33 +0530

    __init__bootstrap

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.slide-effect').mouseleave(function(){
-    $(this).removeClass('clicked');
-  }).click(function(){
-    $(this).addClass('clicked').html($(this).html());
-  });
-});
